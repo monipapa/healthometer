08c26435cdfe8f063c7ecaf98eae13fd2ca7a78b
Monika Papa <m.papa@unizkm.edu.al>
2021-06-10 15:24:18 +0200

    Unit converter

--- healthometer-core/src/main/resources/db/sample-data/V2_1_5__ddl_tbl_unit_subcategories.sql
+++ healthometer-core/src/main/resources/db/sample-data/V2_1_5__ddl_tbl_unit_subcategories.sql
@@ -5,4 +5,4 @@ VALUES (1, 1, 'Kilogram', 'kg', now(), now(), NULL, NULL),
        (2, 1, 'Gram', 'g', now(), now(), NULL, NULL),
        (3, 3, 'Centimeter', 'cm', now(), now(), NULL, NULL),
        (4, 2, 'Calories', 'cal', now(), now(), NULL, NULL),
-       (5, 3, '', '', now(), now(), NULL, NULL);
+       (5, 3, 'Bmi', 'bmi', now(), now(), NULL, NULL);
